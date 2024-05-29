f3cc278dbf88cf1ed97a6fafc2a15bdfee5a4993
Victor Jung <jungvi@iis.ee.ethz.ch>
2024-05-29 17:43:02 +0200

    Remove nn.Sequential wrapping for PACTSoftmax replacement pass

--- editing/fx/passes/pact/approximate.py
+++ editing/fx/passes/pact/approximate.py
@@ -51,7 +51,7 @@ def replSoftmax(gm : fx.GraphModule, match : Match, mode: str, act_cfg: dict, **
     if mode == "I-BERT":
         # Insert unsigned activation before softmax
         # replacement_class = nn.Sequential(PACTAsymmetricAct(**default_act_kwargs), PACTSoftmax(**kwargs))
-        replacement_class = nn.Sequential(PACTSoftmax(**kwargs))
+        replacement_class = PACTSoftmax(**kwargs)
     else:
         cfg = {**default_act_kwargs, **kwargs}
 
